dc6461327733017f1999f92c7eeebd88422d504e
PrakashKrishnanR <prakashkrishnan.official@gmail.com>
2018-10-10 01:17:26 +0530

    Starter Page Completed

--- OnlineShopping/src/main/java/com/prad/OnlineShopping/controller/PageController.java
+++ OnlineShopping/src/main/java/com/prad/OnlineShopping/controller/PageController.java
@@ -13,9 +13,31 @@ public class PageController {
 
 		ModelAndView mv = new ModelAndView("page");
 
-		mv.addObject("greetings", "Welcome to Spring web MVC");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 
+	@RequestMapping(value = "/about")
+
+	public ModelAndView about() {
+
+		ModelAndView mv = new ModelAndView("page");
+
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value ="/contact")
+
+	public ModelAndView contact() {
+
+		ModelAndView mv = new ModelAndView("page");
+
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
 	
 }
